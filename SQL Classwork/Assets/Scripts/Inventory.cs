52072dc534070eba84a0239079c0cda6cc2d295a
Unknown <azzhayman@gmail.com>
2018-09-03 11:53:09 +1000

    Weapon dictionary setup

---
+++ SQL Classwork/Assets/Scripts/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public string[] itemList; // string list of all out items from the database
+    public bool loaded;
+    public Vector2 scr;
+    public Dictionary<int, Weapon> weapons = new Dictionary<int, Weapon>();
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadItemData());
+    }
+
+    private void OnGUI()
+    {
+        scr.x = Screen.width / 16;
+        scr.y = Screen.height / 9;
+
+        if (!loaded)
+        {
+            GUI.Box(new Rect(scr.x * 0, scr.y * 0, scr.x * 16, scr.y), "Loading...");
+        }
+    }
+
+    IEnumerator LoadItemData()
+    {
+        WWW itemDataURL = new WWW("http://localhost/sqlsystem/itemdata.php"); // run out itemdata php script
+        yield return itemDataURL;
+        string textDataString = itemDataURL.text; // pull all items through 1 string
+        string[] items = textDataString.Split('#'); // split the one long string into strings for each item
+        itemList = new string[items.Length - 1]; // set our itemlist to one less the length of items
+        for (int i = 0; i < itemList.Length; i++)
+        {
+            string[] itemDataSplit = itemList[i].Split('|');
+            // splitting string into parts, then creating a new weapon with the data
+            Weapon weapon = new Weapon(int.Parse(itemDataSplit[0]), itemDataSplit[1], int.Parse(itemDataSplit[2]),
+                int.Parse(itemDataSplit[3]), float.Parse(itemDataSplit[4]), float.Parse(itemDataSplit[5]),
+                itemDataSplit[6], itemDataSplit[7], itemDataSplit[8]);
+            // add the weapon to the dictionary using the id number
+            weapons.Add(weapon.id, weapon);
+            Debug.Log(weapons[i].name);
+        }
+        loaded = true;
+    }
+}
---
+++ SQL Classwork/Assets/Scripts/Item.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Item // generic class for items
+{
+    // all items need an ID, a Name, an Icon and a MeshName
+    public int id { get; set; }
+    public string name { get; set; }
+    public Texture2D iconName { get; set; }
+    public GameObject meshName { get; set; }
+}
+
+public class Weapon : Item
+{
+    public int clipSize { get; set; }
+    public int damage { get; set; }
+    public float fireRate { get; set; }
+    public float weaponRange { get; set; }
+    public string ammoType { get; set; }
+
+    public Weapon(int weaponId, string weaponName, int weaponClipSize, int weaponDamage, 
+        float weaponFireRate, float weaponFireRange, string weaponAmmoType, string weaponIconName, 
+        string weaponMeshName)
+    {
+        // item based set up
+        name = weaponName;
+        id = weaponId;
+        iconName = Resources.Load("Icon/" + weaponIconName) as Texture2D; // pulling texture from folder
+        meshName = Resources.Load("Prefabs/" + weaponMeshName) as GameObject; // pulling mesh from folder
+        // weapon based set up
+        clipSize = weaponClipSize;
+        damage = weaponDamage;
+        fireRate = weaponFireRate;
+        weaponRange = weaponFireRange;
+        ammoType = weaponAmmoType;
+    }
+}
